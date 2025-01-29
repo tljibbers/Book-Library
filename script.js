38abd2e6286997d02965f26c522e0954cec47410
tljibbers <aahijoh@gmail.com>
2025-01-29 12:47:58 -0800

    made it so that the text color changes when button is clicked, and delete function works properly, deleting the element in the array as well

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const myLibrary = [];
 const myLibraryDOM = [];
+let currentValue = 0;
 const mainBody = document.querySelector("body");
 const selector = document.querySelector(".gridContainer");
 const buttonClick = document.querySelector(".newBookButton");
 const submitForm = document.querySelector(".formContainer");
 const closeClick = document.querySelector(".closeButton");
 
+const form = document.getElementById('form');
+const title = document.getElementById('title').value;
+const author = document.getElementById('author').value;
+const pages = document.getElementById('pages').value;
+const read = document.getElementById('Read');
+const notRead = document.getElementById('NotRead');
+
+
+
+const test = document.querySelector('#title').value;
 
 
 function book(title, author, pages, haveRead)
@@ -40,19 +51,102 @@ function addBookToLibrary(newBookTitle, newBookAuthor, newBookPages, newBookHave
 
 function displayBooks(myLibrary, myLibraryDOM)
 {
-    for (let i in myLibrary)
-    {
         const bookObject = document.createElement("div");
-        const bookTitle = document.createTextNode(myLibrary[i].title);
-        const bookAuthor = document.createTextNode(myLibrary[i].author);
-        const bookPages = document.createTextNode(myLibrary[i].pages);
-        bookObject.appendChild(bookTitle);
-        bookObject.appendChild(bookAuthor);
-        bookObject.appendChild(bookPages);
-        bookObject.style.borderStyle = "dotted";
-        myLibraryDOM.push(bookObject)
+        const bookObjectHolder = document.createElement("div");
+        const bookObjectTitleHold = document.createElement("div");
+        const bookObjectAuthorHold = document.createElement("div");
+        const bookObjectPagesHold = document.createElement("div");
+        const bookDeleteButton = document.createElement("button");
+        const bookReadToggleButton = document.createElement("button");
+        bookDeleteButton.innerHTML = 'Delete';
+        
+
+        if(read.checked == true)
+        {
+            bookReadToggleButton.innerHTML = 'Have Read';
+            bookReadToggleButton.style.backgroundColor = '#5dbea3';
+        }
+        else
+        {
+            bookReadToggleButton.innerHTML = 'Have Not Read';
+            bookReadToggleButton.style.backgroundColor = '#dd7973'
+        }
+
+        const bookTitle = document.createTextNode('Title:' + " " + myLibrary[myLibrary.length - 1].title);
+        const bookAuthor = document.createTextNode('Author:' + " " + myLibrary[myLibrary.length - 1].author);
+        const bookPages = document.createTextNode('Pages:' + " " + myLibrary[myLibrary.length - 1].pages);
+
+        bookObjectHolder.appendChild(bookObjectTitleHold);
+        bookObjectHolder.appendChild(bookObjectAuthorHold);
+        bookObjectHolder.appendChild(bookObjectPagesHold);
+        bookObjectHolder.appendChild(bookReadToggleButton);
+        bookObjectHolder.appendChild(bookDeleteButton);
+        
+
+        bookObjectTitleHold.appendChild(bookTitle);
+        bookObjectAuthorHold.appendChild(bookAuthor);
+        bookObjectPagesHold.appendChild(bookPages);
+
+        bookObject.appendChild(bookObjectHolder);
+
+        bookObject.style.borderStyle = "solid";
+        bookObject.style.borderWidth = "thin";
+        bookObject.style.boxShadow = '5px 5px 5px 5px darkred'
+        bookObject.style.marginLeft = "20px";
+        bookObject.style.backgroundColor = "white"
+        bookObject.style.display = "flex";
+        bookObject.style.flexDirection = "column";
+        bookObject.style.fontSize = '30px';
+        bookObject.style.gap = '10px';
+
+        bookObjectHolder.style.display = 'flex';
+        bookObjectHolder.style.flexDirection = 'column';
+        bookObjectHolder.style.paddingLeft = '3px';
+        bookObjectHolder.style.gap = '20px';
+        bookObjectHolder.style.margin = 'auto';
+        bookObjectHolder.style.backgroundColor = 'white';
+
+        bookDeleteButton.style.borderRadius = '5px';
+        bookDeleteButton.style.height = '40px';
+        bookDeleteButton.style.width = '230px';
+        
+        
+
+        bookReadToggleButton.style.borderRadius = '5px';
+        bookReadToggleButton.style.height = '40px';
+        bookReadToggleButton.style.width = '230px';
+
+        myLibraryDOM.push(bookObject);
+        
         selector.appendChild(bookObject);
-    }
+
+        bookDeleteButton.onclick = function()
+        {
+            bookObject.remove();
+            const currentIndex = myLibraryDOM.indexOf(bookObject);
+            myLibrary.splice(currentIndex, 1);
+            myLibraryDOM.splice(currentIndex, 1);
+            console.log(myLibrary.length);
+
+        }
+
+        bookReadToggleButton.onclick = function()
+        {
+            if(read.checked == true)
+            {
+                bookReadToggleButton.style.backgroundColor = '#dd7973'
+                bookReadToggleButton.innerHTML = 'Have Not Read';
+                read.checked = false;
+
+                console.log(read.checked);
+            }
+            else
+            {
+                bookReadToggleButton.style.backgroundColor = '#5dbea3';
+                bookReadToggleButton.innerHTML = 'Have Read';
+                read.checked = true;
+            }
+        }
 }
 
 function displayForm()
@@ -65,17 +159,46 @@ function hideForm()
     submitForm.style.visibility = "hidden";
 }
 
-addBookToLibrary("yes", "yes", "yes", true, myLibrary);
-addBookToLibrary("no", "no", "no", true, myLibrary);
-
-displayBooks(myLibrary, myLibraryDOM);
 
 
 buttonClick.onclick = function() {
+    
     console.log("yay!");
     displayForm()
 }
 
 closeClick.onclick = function() {
     hideForm()
+}
+
+form.addEventListener('submit', e =>{
+    if(title == '' || title == null)
+    {
+        e.preventDefault()
+    }
+    if(author == '' || author == null)
+    {
+        e.preventDefault()
+    }
+    if(pages == '' || pages == null)
+    {
+        e.preventDefault()
+    }
+
+
+    addBooksDOM();
+    displayBooks(myLibrary, myLibraryDOM);
+
+    
+});
+
+function addBooksDOM() {
+    if(read.checked == true)
+    {
+        addBookToLibrary(document.getElementById('title').value, document.getElementById('author').value, document.getElementById('pages').value, true, myLibrary)
+    }
+    else
+    {
+        addBookToLibrary(document.getElementById('title').value, document.getElementById('author').value, document.getElementById('pages').value, false, myLibrary)
+    }
 }
